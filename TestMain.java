8263e299e818cc221cf3ab70c5994f9bb56d9c93
zyycode <501058587@qq.com>
2024-11-05 22:40:37 +0800

    master zyy

---
+++ TestMain.java
@@ -0,0 +1,13 @@
+/**
+ * @Author: Zz
+ * @Date: 2024/11/05/22:36
+ * @Description: 致敬
+ */
+public class TestMain {
+    public static void main(String[] args) {
+
+        System.out.println("master分支上的更改");
+
+        System.out.println("master 同学更改了....");
+    }
+}
